084ea0f2b21737a48480a77d4e1544ddae5c12ff
LastBit97 <diman01.97@gmail.com>
2022-11-03 13:12:06 +0300

    add tests

---
+++ Tests/CircleTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AreaLib.Tests
+{
+    [TestClass]
+    public class CircleTest
+    {
+        [TestMethod]
+        public void CircleAreaTestMethod()
+        {
+            //Arrange
+            double radius = 4;
+            double expected = 50.2655;
+
+            //Act
+            Circle circle = new Circle(radius);
+            double actual = circle.GetArea();
+
+            //Assert
+            Assert.AreEqual(expected, actual, "Error");
+        }
+    }
+}
---
+++ Tests/TriangleTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AreaLib.Tests
+{
+    [TestClass]
+    public class TriangleTest
+    {
+        [TestMethod]
+        public void TriangleAreaTestMethod()
+        {
+            //Arrange
+            double a = 3;
+            double b = 4;
+            double c = 5;
+            double expected = 6;
+
+            //Act
+            Triangle triangle = new Triangle(a, b, c);
+            double actual = triangle.GetArea();
+
+            //Assert
+            Assert.AreEqual(expected, actual, "Error");
+        }
+
+        [TestMethod]
+        public void TriangleRectangularTestMethod()
+        {
+            //Arrange
+            double a = 3;
+            double b = 4;
+            double c = 5;
+            bool expected = true;
+
+            //Act
+            Triangle triangle = new Triangle(a, b, c);
+            bool actual = triangle.isRectangular();
+
+            //Assert
+            Assert.AreEqual(expected, actual, "Error");
+        }
+    }
+}
